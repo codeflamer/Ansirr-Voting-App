0ae7dbc44ba6bdeb42c3e4b242b4a5879f9ea25b
Tolulope <emryzs01@gmail.com>
2021-12-18 15:27:35 +0300

    made the data able to be posted

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import PositionCard from '../components/PositionCard';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+// import axios from 'axios';
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Loader from 'react-loader-spinner';
+const axios = require('axios')
   
 const Home = () => {
   const [data,setData] = useState();
   const [error,setErrorMessage] = useState();
   const [success,setSuccessMessage] = useState();
   const [returnMsg,setReturnMsg] = useState(false);
+  const randomId = (Math.ceil(Math.random()*10)).toString();
+
   let navigate = useNavigate();
 
     const handleSubmit = (e)=>{
         e.preventDefault();
-
+        setErrorMessage(false);
         const [vicePresidentPositionId,selectedvicePresidentcandidateId] = e.target.VicePresident.value.split('@');
         const [presidentPositionId,SelectedpresidentcandidateId] = e.target.President.value.split('@');
-
+        console.log(randomId);
         let dataToPost = {
-              id:'242212',
+              id:randomId,
               data:[
                 {
                   office: presidentPositionId,
@@ -28,28 +31,22 @@ const Home = () => {
                 {
                   office: vicePresidentPositionId,
                   candidate:selectedvicePresidentcandidateId
-                },
+                }
               ]
         }
 
-        console.log(JSON.stringify(dataToPost));
-
-        axios.post('https://anssir-voting-api.herokuapp.com/vote', dataToPost)
+        axios.post('http://anssir-voting-api.herokuapp.com/vote', dataToPost)
         .then(function (response) {
-          console.log(response.data);
           setSuccessMessage(response.data);
-          console.log('Success message')
+          navigate('/voted');
         })
         .catch(function (error) { 
-          // handle error
-          console.log('Error Here');
           console.log(error);
-          // setErrorMessage(error.data);
+          setErrorMessage('This user already Voted');
         })
 
-        if (success){
+        if(success){
           setErrorMessage(false);
-          navigate('/voted');
         }
         else {
           setErrorMessage(true);
@@ -72,7 +69,7 @@ const Home = () => {
         <div>
             <h1 className="font-bold text-2xl">Your Vote is Secure, Your Vote Counts</h1>
             <p className="mt-3 text-gray-500">You can only vote for one candidate</p>
-            {error && <p className='text-red-600'>Error has occured</p>}
+            {error && <p className='text-red-600'>{error}</p>}
             {data ?
               <form onSubmit={handleSubmit}>
                 {
